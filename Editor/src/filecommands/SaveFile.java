9c4399346fd54f7d71eeea052ac29eccf84f6df4
Anshul Kapoor <axk4789@rit.edu>
2013-04-16 15:13:51 -0400

    Save functionality working. Alerts user if an unopened file is being saved.

--- Editor/src/filecommands/SaveFile.java
+++ Editor/src/filecommands/SaveFile.java
@@ -3,6 +3,7 @@ package filecommands;
 import commands.FileCommand;
 import gui.*;
 import javax.swing.*;
+
 import java.io.*;
 public class SaveFile implements FileCommand{
 	
@@ -13,5 +14,29 @@ public class SaveFile implements FileCommand{
 	
 	public void execute(){
 		//perform validation, then copy buffer to .html file
+
+		int tabCount = gui.tabbedPane.getTabCount();
+		if(tabCount == 0){
+        	//String message = "Cannot save unopened file!";
+        	//JOptionPane alertMessage = new JOptionPane(message);
+        	JOptionPane.showMessageDialog(gui, "Cannat save unopened file", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+		else{
+		JFileChooser chooser = new JFileChooser();
+		int returnVal = chooser.showSaveDialog(null);
+        		
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			try{
+			File file = chooser.getSelectedFile();
+            BufferedWriter wr = new BufferedWriter( new FileWriter(file));
+            
+            String line = gui.getText();
+            wr.write(line);
+            wr.close();
+            
+            }
+            catch(Exception ex){}
+        }
+	}
 	}
 }
